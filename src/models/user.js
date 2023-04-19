e4db48b3985a694bb2812cc380dcccf01654bf96
Jardeylson jacinto <jardeylsong.m@gmail.com>
2023-04-19 10:47:46 -0300

    feat: Defining the user's schema and model using Mongoose

---
+++ src/models/user.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 8,
+  }
+})
